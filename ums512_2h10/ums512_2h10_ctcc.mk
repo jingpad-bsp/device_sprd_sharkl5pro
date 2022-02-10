13403715b73ce344c7f115e890fe742eff59a882
deadman96385 <seanhoyt963@gmail.com>
2022-02-10 06:46:36 +0000

    Initial Import

---
+++ ums512_2h10/ums512_2h10_ctcc.mk
@@ -0,0 +1,7 @@
+include device/sprd/sharkl5Pro/ums512_2h10/ums512_2h10_Natv.mk
+
+PRODUCT_REVISION := ctcc
+include $(APPLY_PRODUCT_REVISION)
+
+# Override
+PRODUCT_NAME := ums512_2h10_ctcc
---
+++ ums512_2h10/ums512_2h10_nosec.mk
@@ -0,0 +1,7 @@
+include device/sprd/sharkl5Pro/ums512_2h10/ums512_2h10_Natv.mk
+
+# Override
+PRODUCT_NAME := ums512_2h10_nosec
+
+#no secure boot
+BOARD_SECBOOT_CONFIG := false
